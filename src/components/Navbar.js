673b678147ac4ffc5768cc5daf2188e70008cb47
Mauriciods07 <keigro@hotmail.es>
2021-07-06 11:52:37 -0500

    esqueleto

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import logo from "../images/logo.png";
+import { ShoppingCart } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+      flexGrow: 1,
+      marginBottom: "7rem",
+  },
+  appBar: {
+      backgroundColor: "green",
+      boxShadow: "none",
+  },
+  grow: {
+      flexGrow: 1,
+  },
+  button: {
+      marginLeft: theme.spacing(2),
+  },
+  image: {
+      marginRight: "10px",
+      height: "2.5rem",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <img src={logo} className={classes.image}/>
+          </IconButton>
+          <Typography variant="h6" component="p">
+            Hola, Usuario
+          </Typography>
+          <div className={classes.button}>
+              <Button variant="contained" color="primary">
+              <strong>Registrarse</strong>
+              </Button>
+          <IconButton aria-label="show cart items" color="inherit">
+              <ShoppingCart fontSize="large" color="primary"/>
+          </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
